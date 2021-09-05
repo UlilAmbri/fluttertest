df0d8b8c361fe93b7854d00d2d4ed913f0488065
Ulil Ambri <ulilambri9888@gmail.com>
2021-09-05 12:55:12 +0700

    mengganti title

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login Google',
+      title: 'Login Hoax',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
